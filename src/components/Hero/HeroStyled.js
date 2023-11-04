d350b59089ae2975646db7e8f9b00d0ec82c2730
Andrey1914 <anlikajw@gmail.com>
2023-11-04 19:02:12 +0100

    added hero img

---
+++ src/components/Hero/HeroStyled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Hero = styled.div`
+  height: 500px;
+  background: linear-gradient(67deg, #111 6.82%, rgba(17, 17, 17, 0) 100%),
+    url("https://globalalphacapital.cclgroup.com/wp-content/uploads/2023/02/GACM_Comm_2023-02-23_Banner.jpg"),
+    lightgray 295.604px -4.18px / 92.376% 104.171% no-repeat;
+  background-size: cover;
+`;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import { Hero } from "../Hero/HeroStyled";
 import MainContent from "../MainContent/MainContent";
 
 export default function Layout() {
   return (
     <>
       <Header />
+      <Hero />
       <MainContent />
       <Footer />
     </>
