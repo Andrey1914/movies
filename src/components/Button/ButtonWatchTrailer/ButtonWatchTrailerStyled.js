2aba40de6993f6d4e83ec073766b08aac1f65697
Andrey1914 <anlikajw@gmail.com>
2024-03-20 14:14:59 +0100

    painted youtube button

--- src/components/Button/ButtonWatchTrailer/ButtonWatchTrailerStyled.js
+++ src/components/Button/ButtonWatchTrailer/ButtonWatchTrailerStyled.js
@@ -2,7 +2,9 @@ import styled from "styled-components";
 import { FaYoutube } from "react-icons/fa";
 import { ButtonStyled } from "../ButtonStyled";
 
-export const YouTubeButton = styled(ButtonStyled)``;
+export const YouTubeButton = styled(ButtonStyled)`
+  background: red;
+`;
 
 export const YouTubeIcon = styled(FaYoutube)`
   margin-right: 0.5rem;
